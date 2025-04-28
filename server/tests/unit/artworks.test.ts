69126e667da6170a0e837523ec59c7a38288be95
BlueSetbyeol <setbyeol.c@gmail.com>
2025-04-28 16:49:39 +0200

    secured forms so informations are as protected or informed as possible

--- server/tests/unit/artworks.test.ts
+++ server/tests/unit/artworks.test.ts
@@ -1,66 +1,20 @@
-// Import the supertest library for making HTTP requests
 import supertest from "supertest";
 
-// Import the Express application
+import "dotenv/config";
+import DatabaseClient from "../../database/client";
 import app from "../../src/app";
 
-// Import databaseClient
-import databaseClient from "../../database/client";
-import type { Result, Rows } from "../../database/client";
-
-// Test suite for the GET /api/items route
 describe("GET /api/artworks", () => {
   it("should fetch artworks successfully", async () => {
-    // Mock empty rows returned from the database
-    const rows = [] as Rows;
-
-    // Mock the implementation of the database query method
-    jest
-      .spyOn(databaseClient, "query")
-      .mockImplementation(async () => [rows, []]);
-
-    // Send a GET request to the /api/artworks endpoint
     const response = await supertest(app).get("/api/artworks");
 
-    // Assertions
     expect(response.status).toBe(200);
-    expect(response.body).toStrictEqual(rows);
+    expect(response.body).toBeInstanceOf(Array);
+    expect(response.body.length).toBeGreaterThanOrEqual(0);
+    expect(response.body[0]).toHaveProperty("id");
   });
 });
 
-// Test suite for the GET /api/artwork/:id route
-describe("GET /api/artwork/:id", () => {
-  it("should fetch a single artwork successfully", async () => {
-    // Mock rows returned from the database
-    const rows = [{}] as Rows;
-
-    // Mock the implementation of the database query method
-    jest
-      .spyOn(databaseClient, "query")
-      .mockImplementation(async () => [rows, []]);
-
-    // Send a GET request to the /api/items/:id endpoint
-    const response = await supertest(app).get("/api/artwork/1");
-
-    // Assertions
-    expect(response.status).toBe(200);
-    expect(response.body).toStrictEqual(rows[0]);
-  });
-
-  it("should fail on invalid id", async () => {
-    // Mock empty rows returned from the database
-    const rows = [] as Rows;
-
-    // Mock the implementation of the database query method
-    jest
-      .spyOn(databaseClient, "query")
-      .mockImplementation(async () => [rows, []]);
-
-    // Send a GET request to the /api/artwork/:id endpoint with an invalid ID
-    const response = await supertest(app).get("/api/artwork/0");
-
-    // Assertions
-    expect(response.status).toBe(404);
-    expect(response.body).toEqual({});
-  });
+afterAll(async () => {
+  await DatabaseClient.end();
 });
